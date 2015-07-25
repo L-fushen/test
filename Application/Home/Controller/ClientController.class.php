07cc9da594edbbefa60d90fbbe0ef6878965a0a5
shen <786699380@qq.com>
2015-07-25 18:18:29 +0800

    Default

--- Application/Home/Controller/ClientController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class ClientController extends Controller {
-    public function index(){
-        $this->display(APP_PATH.'Home/View/Client-list.html');
-    }
-}
--- Application/Home/Controller/ListController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class ListController extends Controller {
-    public function index(){
-        $this->display(APP_PATH.'Home/View/list.html');
-    }
-}
